bd3580d9865ec8de8410dc34e2563995415d18ca
Eric Fabroa <eric.fabroa@ycdsbk12.ca>
2020-12-18 15:06:13 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,59 @@
+"""
+Draws a Rectangle to the screen
+"""
+
+import pygame
+
+# Define some colors
+black    = (   0,   0,   0)
+white    = ( 255, 255, 255)
+green    = (   0, 255,   0)
+red      = ( 255,   0,   0)
+
+pygame.init()
+ 
+# Set the height and width of the screen
+size = [700, 500]
+screen = pygame.display.set_mode(size)
+
+pygame.display.set_caption("A Box")
+
+#Loop until the user clicks the close button.
+done = False
+
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+
+# Starting position of the rectangle
+rect_x = 50
+rect_y = 50
+
+# -------- Main Program Loop -----------
+while done == False:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            print("User asked to quit.")
+            done = True
+        elif event.type == pygame.KEYDOWN:
+            print("User pressed a key.")
+        elif event.type == pygame.KEYUP:
+            print("User let go of a key.")
+        elif event.type == pygame.MOUSEBUTTONDOWN:
+            print("User pressed a mouse button")
+
+    # Set the screen background
+    screen.fill(white)
+
+    # Draw the rectangle
+    pygame.draw.rect(screen, red, [rect_x, rect_y, 50, 50])
+
+    
+    # Limit to 20 frames per second
+    clock.tick(20)
+
+    # Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+    
+# Be IDLE friendly. If you forget this line, the program will 'hang'
+# on exit.
+pygame.quit()
